e5eda905e782e49a60ec085550631d980d4f9142
cariolano <cariolanosolar@gmail.com>
2019-11-23 23:25:34 -0200

    modifiquei apenas nome de atributos para portugues

--- src/MVC/Contact.java
+++ src/MVC/Contact.java
@@ -5,23 +5,23 @@ import java.util.List;
 
 public class Contact {
 
-	private String emailAddress;
-	private String faxNumber;
-	private String name;
+	private String email;
+	private String fax;
+	private String nome;
 	private String primaryContactMethod;
 	public List<ContactGroup> groups = new ArrayList<ContactGroup>();
 	public List<AddressBook> addressBooks = new ArrayList<AddressBook>();
 	
-	public String getEmailAdress() {
-		return emailAddress;
+	public String getEmail() {
+		return email;
 	}
 	
-	public String getFaxNumber() {
-		return faxNumber;
+	public String getFax() {
+		return fax;
 	}
 	
-	public String getName() {
-		return name;
+	public String getNome() {
+		return nome;
 	}
 	
 	public String getPrimaryContactMethod() {
@@ -30,15 +30,15 @@ public class Contact {
 	
 	
 	public void setEmailAdress(String emailAdress) {
-		this.emailAddress = emailAdress;
+		this.email = email;
 	}
 	
-	public void setFaxNumber(String faxNumber) {
-		this.faxNumber = faxNumber;
+	public void setFax(String fax) {
+		this.fax = fax;
 	}
 	
-	public void setName(String name) {
-		this.name = name;
+	public void setNome(String nome) {
+		this.nome = nome;
 	}
 	
 	public void setPrimaryContactMethod(String primaryContactMethod) {
